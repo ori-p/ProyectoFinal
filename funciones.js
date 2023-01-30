a6084aae1042eb92037b18fa7fbe880880d7f852
ori <oriipagalday>
2023-01-30 16:49:55 -0300

    proyecto final

---
+++ funciones.js
@@ -0,0 +1,10 @@
+document.getElementById("value_horizontal").addEventListener("click" , function(){
+
+    let datos = document.getElementById("#Datos").ariaValueText= String;
+    document.getElementById("#Datos").innerHTML= datos;
+    document.getElementById("#Datos").style.display = "block";
+
+
+}
+
+)
