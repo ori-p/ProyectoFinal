51f2f259aa2453cc7bb029c79daec379449fd5f6
ori <oriipagalday>
2023-02-06 18:38:39 -0300

    Proyecto Final

--- funciones.js
+++ funciones.js
@@ -1,10 +1,33 @@
-document.getElementById("value_horizontal").addEventListener("click" , function(){
+/**document.getElementById("CafeMartinez").addEventListener('click', function(){
+  if(document.getElementById('estilo').getAttribute('href')=="css/stilos.css" ){
+      document.getElementById('estilo').setAttribute('href', "css/violeta.css");
+      document.getElementById("CafeMartinez").innerHTML = "CafeMartinez" ;
+      console.log("Se cambio al modo oscuro");
+  }
+  else{
+      document.getElementById('estilo').setAttribute('href', "css/stilos.css");
+      document.getElementById("CafeMartinez").innerHTML = 'Cambiar a violeta oscuro';
+      console.log("Se cambio al modo claro.");
+  }
+}) ;
+*/
 
-    let datos = document.getElementById("#Datos").ariaValueText= String;
-    document.getElementById("#Datos").innerHTML= datos;
-    document.getElementById("#Datos").style.display = "block";
-
-
+document.getElementById("CafeMartinez").addEventListener('click' , function() {
+   
+  document.body.style.backgroundColor = 'rgb(101, 89, 110)'
 }
-
 )
+
+
+document.getElementById("Donatozurlo").addEventListener('click' , function() {
+   
+  document.body.style.backgroundColor = 'rgb(219, 213, 224)'
+}
+)
+
+document.getElementById("Natachanina").addEventListener('click' , function() {
+   
+  document.body.style.backgroundColor = 'rgb(210, 181, 236)'
+}
+)
+
